e79a0dd69dec34fd6538672e975b688d308ace16
Tiago <andrade.tiago166@gmail.com>
2021-03-22 15:47:38 -0300

    Página completa adicionada

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+      },
+});
